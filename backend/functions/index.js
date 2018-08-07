253470f203abd804f566ee527d873bc517eb2887
iulian <iuliancl2000@yahoo.com>
2018-08-07 18:44:28 +0300

    fix tournament context menu options condition

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -26,8 +26,30 @@ exports.sendSubscriberNotification = functions.firestore
 
       var message = {
         data: {
-          score: '850',
-          time: '2:45',
+          message: 'This is a notification with results published'
+        },
+        topic: topicId
+      };
+
+      // Send a message to devices subscribed to the provided topic.
+      return admin.messaging().send(message)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+          return 0;
+        })
+        .catch((error) => {
+          console.log('Error sending message:', error);
+        });
+    });
+
+exports.sendSubscriberNotification_FDB = functions.database.ref('/result/{topicId}/{docId}')
+    .onCreate((snap, context) => {
+      const topicId = context.params.topicId;
+      console.log('We have a new results published for tournament:', topicId);
+
+      var message = {
+        data: {
           message: 'This is a notification with results published'
         },
         topic: topicId
@@ -93,11 +115,84 @@ exports.sendRegistrationNotification = functions.firestore
             });
         });
 
+exports.sendRegistrationNotification_FDB = functions.database.ref('/subscription/{topicId}/{docId}')
+    .onCreate((snap, context) => {
+      const newValue = snap.val();
+      const topicId = context.params.topicId;
+
+      if (newValue.intent !== 'participant') {
+        return 0;
+      }
+      console.log('We have a new registration for tournament:', topicId);
+
+      let creatorUid;
+      let creatorToken;
+
+      return admin.database()
+        .ref('tournament')
+        .child(topicId)
+        .on("value", function(doc) {
+            creatorUid = doc.val().creator;
+            console.log('Got creator uid: ' + creatorUid);
+            return admin.database().ref('user').child(creatorUid).on("value", function(snap) {
+              creatorToken = snap.val().token;
+              console.log('Got creator token: ' + creatorToken);
+              var message = {
+                data: {
+                  command: 'registration',
+                  tournament_name: doc.val().fullName,
+                  tournament_id: topicId,
+                  egf_pin: newValue.egfPin,
+                  message: 'This is a notification for a new registration'
+                },
+                token: creatorToken
+              };
+
+              // Send a message to tournament creator device.
+              return admin.messaging().send(message)
+                .then((response) => {
+                  // Response is a message ID string.
+                  console.log('Successfully sent message:', response);
+                  return 0;
+                })
+                .catch((error) => {
+                  console.log('Error sending message:', error);
+                });
+              });
+            });
+        });
+
 exports.sendTournamentMessage = functions.firestore
     .document('/tournament/{topicId}/message/{docId}')
     .onCreate((snap, context) => {
       const topicId = context.params.topicId;
       const newMessage = snap.data();
+      console.log('We have a new message to participant in tournament:', topicId);
+
+      var message = {
+        data: {
+          message: newMessage.message,
+          tournament_name: newMessage.from
+        },
+        topic: topicId
+      };
+
+      // Send a message to devices subscribed to the provided topic.
+      return admin.messaging().send(message)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+          return 0;
+        })
+        .catch((error) => {
+          console.log('Error sending message:', error);
+        });
+    });
+
+exports.sendTournamentMessage_FDB = functions.database.ref('/message/{topicId}/{docId}')
+    .onCreate((snap, context) => {
+      const topicId = context.params.topicId;
+      const newMessage = snap.val();
       console.log('We have a new message to participant in tournament:', topicId);
 
       var message = {
